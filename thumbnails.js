758cc290cc12241cf23a60e74b9b5f7da3e8411a
gnsp <sir.gnsp@gmail.com>
2019-09-28 02:57:30 +0530

    added lg

--- thumbnails.js
+++ thumbnails.js
@@ -9,13 +9,16 @@ fs.readFile('gallery.json', 'utf8', function (err, str) {
         let basename = filename.split('/')[1];
         let sm = 'sm/'+basename;
         let md = 'md/'+basename;
+        let lg = 'lg/'+basename;
         jimp.read(filename).then(img => {
             img.contain(512, 512).write(md);
             img.contain(256, 256).write(sm);
+            img.contain(720, 720).write(lg);
             console.log('processing '+basename);
         });
         gallery[i].sm = sm;
         gallery[i].md = md;
+        gallery[i].lg = lg;
     }
     fs.writeFile('gallery.json', JSON.stringify(gallery, null, 2), _ => 1);
 });
